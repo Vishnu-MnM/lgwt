0c242e8bd2d4fbeea68097c362d1171cfdcc2244
Vishnu_MnM <sunny_4210@proton.me>
2026-09-10 11:30:15 -0400

    First commit, intro to tests, just wrote the basic code, needs further improvement to the code (adding multiple languages)

---
+++ hello_world/v5/hello_test.go
@@ -0,0 +1,25 @@
+// Refactor of the test in v4
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func (t *testing.T) {
+		got := Hello("Sunny")
+		want := "Hello, Sunny"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
